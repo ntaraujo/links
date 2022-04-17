43bd3ed77d27a563d79f7ee8a78ea4093f720b2a
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-04-17 15:54:58 -0300

    fix onclick

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ function App() {
                             title={item.title}
                             subtitle={item.subtitle}
                             link={item.link}
-                            cover={item.image}>
+                            cover={item.image}
+                            onclick={item.onclick}>
                             </Card>
                         )
                     })
