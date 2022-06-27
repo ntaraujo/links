686d39e3665faaeaf1ba090e81f956547cfafadd
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-06-27 20:09:11 -0300

    invert _blank <-> _self

--- src/assets/items.js
+++ src/assets/items.js
@@ -11,42 +11,42 @@ const items = [
     "subtitle": "📸",
     "image": instagram,
     "link": "https://instagram.com/ntaraujo1",
-    "target": "_self"
+    "target": "_blank"
 },
 {
     "title": "Textos",
     "subtitle": "✍️",
     "image": text,
     "link": "https://instagram.com/neitanto",
-    "target": "_self"
+    "target": "_blank"
 },
 {
     "title": "GitHub",
     "subtitle": "👨‍💻",
     "image": github,
     "link": "https://github.com/ntaraujo",
-    "target": "_self"
+    "target": "_blank"
 },
 {
     "title": "LinkedIn",
     "subtitle": "👔",
     "image": linkedin,
     "link": "https://www.linkedin.com/in/ntaraujo/",
-    "target": "_self"
+    "target": "_blank"
 },
 {
     "title": "Pix",
     "subtitle": "😎",
     "image": pix,
     "link": "javascript:navigator.clipboard.writeText('e48fcf72-c679-4cd4-a018-545435c4052e');alert('Copiado 👍')",
-    "target": "_blank"
+    "target": "_self"
 },
 {
     "title": "Convite Inter",
     "subtitle": "✉️",
     "image": inter,
     "link": "https://bancointer.onelink.me/eiWM/6zb5b9ux",
-    "target": "_self"
+    "target": "_blank"
 }]
 
 export default items
